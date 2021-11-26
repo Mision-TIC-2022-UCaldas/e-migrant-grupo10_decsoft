0ccdd2674fda1b957662541a7e19de1d2d6506a0
juanlgarzon <juan98.lozanog@gmail.com>
2021-11-26 09:38:13 -0500

    iMC

---
+++ iMC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace iMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su peso (Kg):");
+            int peso = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su estatura separada por coma (m):");
+            double estatura = Convert.ToDouble(Console.ReadLine());
+            double mc=peso/(Math.Pow(estatura, 2));
+            Console.WriteLine("Diagnóstico:");
+            if(mc<16){
+                Console.WriteLine("Padece Delgadez severa");
+            }else if(mc>=16 && mc<=16.99){
+                Console.WriteLine("Padece Delgadez moderada");
+            }else if(mc>=17 && mc<=18.49){
+                Console.WriteLine("Padece Delgadez aceptable");
+            }else if(mc>=18.5 && mc<=24.99){
+                Console.WriteLine("Tiene Peso normal");
+            }else if(mc>=25 && mc<=29.99){
+                Console.WriteLine("Padece Sobrepeso");
+            }else if(mc>=30 && mc<=34.99){
+                Console.WriteLine("Padece Obesidad tipo I");
+            }else if(mc>=35 && mc<=39.99){
+                Console.WriteLine("Padece Obesidad tipo II");
+            }else if(mc>=40 && mc<=49.99){
+                Console.WriteLine("Padece Obesidad tipo III o mórbida");
+            }else if(mc>50){
+                Console.WriteLine("Padece Obesidad tipo IV o extrema");
+            }
+        }
+    }
+}
